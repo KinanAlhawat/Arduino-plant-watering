27438d23e20f59f1cae5a58465253155bef19aa8
Kinan Alhawat <kinanalhawat@gmail.com>
2021-04-14 10:22:01 +0200

    test

--- src/Sensor_auslesen/Sensor_auslesen.ino
+++ src/Sensor_auslesen/Sensor_auslesen.ino
@@ -1,21 +1,26 @@
 void setup() {
   Serial.begin(1200);
+  pinMode(4, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(4, HIGH);
+  digitalWrite(13, HIGH);
   int val;
   int val1;
   val = analogRead(A0);
-  //Serial.println(val);
   delay(100);
-  
-  if (val <= 400){
-    digitalWrite(12, HIGH);
-  }else if (val >= 400){
-    digitalWrite(12, LOW);
-  }
-  
+
   val1 = map (val, 596, 283, 0, 100);
   Serial.println(val1);
+  
+  if (val1 <= 50){
+    digitalWrite(12, HIGH);
+  }else if (val1 >= 50){
+    digitalWrite(12, LOW);
+  //  hi
+  }
+  
 }
