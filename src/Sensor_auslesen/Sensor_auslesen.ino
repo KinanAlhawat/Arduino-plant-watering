7d3bfe091826e85abcd52aeb080f12a39cf77df9
Kinan Alhawat <kinanalhawat@gmail.com>
2021-04-14 14:51:59 +0200

    finale version

--- src/Sensor_auslesen/Sensor_auslesen.ino
+++ src/Sensor_auslesen/Sensor_auslesen.ino
@@ -1,26 +1,21 @@
 void setup() {
   Serial.begin(1200);
-  pinMode(4, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);    //Pin 12 steuert das Relais
 }
 
 void loop() {
-  digitalWrite(4, HIGH);
-  digitalWrite(13, HIGH);
   int val;
   int val1;
-  val = analogRead(A0);
-  delay(100);
+  val = analogRead(A0);   //Analogpin A0 lest die Segnale des Sensors aus
 
-  val1 = map (val, 596, 283, 0, 100);
-  Serial.println(val1);
+  val1 = map (val, 596, 283, 0, 100);    //Mit der map() Funktion werwandeln wir die ausgelese Sinale zu Prozentzahlen
+  Serial.println(val1);   
+ // Serial.println(val);  //Falls man die Pumpe mit den normalen Werten statt mit Prozent-Angaben Steuern will
   
-  if (val1 <= 50){
+    if (val1 <= 20){       //Die Pumpe geht 20% Feuchtigkeit an
     digitalWrite(12, HIGH);
-  }else if (val1 >= 50){
+  }else if (val1 >= 20){   //Die Pumpe geht 20% Feuchtigkeit aus
     digitalWrite(12, LOW);
-  //  hi
   }
   
 }
